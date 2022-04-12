dd7b6910133bc994663f158c9ee919879d90b1ea
irfanakbari <irfanakbarihabibi18@gmail.com>
2022-04-12 09:48:51 +0700

    cors

--- router/routes.js
+++ router/routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import login from '../controller/auth.js';
 import edokter from '../controller/edokter.js';
 import middleware from '../middleware/auth.js';
@@ -6,6 +7,7 @@ import etrash from '../controller/etrash.js'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
